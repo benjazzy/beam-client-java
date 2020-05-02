bae3f1a38f9d8c0cb9768845fd45bb7885df0c72
benjazzy <benjazzy64@gmail.com>
2020-05-02 19:35:49 -0400

    Added clear event data.

--- src/main/java/com/mixer/api/resource/chat/events/ClearMessagesEvent.java
+++ src/main/java/com/mixer/api/resource/chat/events/ClearMessagesEvent.java
@@ -1,9 +1,9 @@
 package com.mixer.api.resource.chat.events;
 
 import com.mixer.api.resource.chat.AbstractChatEvent;
-import com.mixer.api.resource.chat.events.data.DeleteMessageData;
+import com.mixer.api.resource.chat.events.data.ClearMessagesData;
 
-public class ClearMessagesEvent extends AbstractChatEvent<DeleteMessageData> {
+public class ClearMessagesEvent extends AbstractChatEvent<ClearMessagesData> {
     public ClearMessagesEvent() {
         this.type = Type.EVENT;
     }
---
+++ src/main/java/com/mixer/api/resource/chat/events/data/ClearMessagesClearer.java
@@ -0,0 +1,13 @@
+package com.mixer.api.resource.chat.events.data;
+
+import com.google.gson.annotations.SerializedName;
+import com.mixer.api.resource.MixerUser;
+
+import java.util.List;
+
+public class ClearMessagesClearer {
+    @SerializedName("user_name") public String username;
+    @SerializedName("user_id") public int userId;
+    @SerializedName("user_roles") public List<MixerUser.Role> userRole;
+    @SerializedName("user_level") public int userLevel;
+}
